1a0cfbf32580d8ef963039c630810ca978a744a2
Jed Duffey <jedduffey@gmail.com>
2023-03-24 20:35:32 -0400

    Page displays seconds elapsed since generate button was clicked

--- trivia-frontend/src/TriviaInputForm.js
+++ trivia-frontend/src/TriviaInputForm.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TriviaInputForm = ({ categoryInput, setCategoryInput, numQuestions, onSubmit }) => {
+    const [elapsedSeconds, setElapsedSeconds] = useState(0);
+    const [timerActive, setTimerActive] = useState(false);
+
+    useEffect(() => {
+        if (timerActive) {
+            const timer = setInterval(() => {
+                setElapsedSeconds((prevElapsedSeconds) => prevElapsedSeconds + 1);
+            }, 1000);
+
+            return () => clearInterval(timer);
+        }
+    }, [timerActive]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTimerActive(true);
+        onSubmit(e);
+    };
+
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="categoryInput">Category:</label>
             <input
                 id="categoryInput"
@@ -14,7 +33,8 @@ const TriviaInputForm = ({ categoryInput, setCategoryInput, numQuestions, onSubm
             <br />
             <button type="submit">
                 {`Generate ${numQuestions} Questions`}
-                </button>
+            </button>
+            {timerActive && <p>Elapsed time: {elapsedSeconds} seconds</p>}
         </form>
     );
 };
