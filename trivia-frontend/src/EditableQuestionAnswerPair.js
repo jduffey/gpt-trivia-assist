8231f80ff1d9dae084e22c1a335d0db6726f5f05
Jed Duffey <jedduffey@gmail.com>
2023-03-25 16:56:56 -0400

    Add styling to components

--- trivia-frontend/src/EditableQuestionAnswerPair.js
+++ trivia-frontend/src/EditableQuestionAnswerPair.js
@@ -11,39 +11,47 @@ const EditableQuestionAnswerPair = ({
 }) => {
     return (
         <div className="question-answer-pair">
-            <label htmlFor={`question-${index}`}>Question {index}:</label>
+            <label htmlFor={`question-${index}`} className="question-label">
+                Q {index}:
+            </label>
             <textarea
                 id={`question-${index}`}
                 value={question}
                 onChange={(event) => onQuestionChange(index, event.target.value)}
+                className="question-textarea"
             />
-            <label htmlFor={`answer-${index}`}>Answer:</label>
+            <label htmlFor={`answer-${index}`} className="answer-label">
+                A:
+            </label>
             <textarea
                 id={`answer-${index}`}
                 value={answer}
                 onChange={(event) => onAnswerChange(index, event.target.value)}
+                className="answer-textarea"
             />
-            <button
-                className={difficulty === 0 ? 'selected-difficulty' : ''}
-                id="save-easy"
-                onClick={() => setDifficulty(index, 0)}
-            >
-                Easy
-            </button>
-            <button
-                className={difficulty === 1 ? 'selected-difficulty' : ''}
-                id="save-med"
-                onClick={() => setDifficulty(index, 1)}
-            >
-                Medium
-            </button>
-            <button
-                className={difficulty === 2 ? 'selected-difficulty' : ''}
-                id="save-difficult"
-                onClick={() => setDifficulty(index, 2)}
-            >
-                Difficult
-            </button>
+            <div className="difficulty-buttons">
+                <button
+                    className={`difficulty-btn ${difficulty === 0 ? 'selected-difficulty' : ''}`}
+                    id="save-easy"
+                    onClick={() => setDifficulty(index, 0)}
+                >
+                    Easy
+                </button>
+                <button
+                    className={`difficulty-btn ${difficulty === 1 ? 'selected-difficulty' : ''}`}
+                    id="save-med"
+                    onClick={() => setDifficulty(index, 1)}
+                >
+                    Medium
+                </button>
+                <button
+                    className={`difficulty-btn ${difficulty === 2 ? 'selected-difficulty' : ''}`}
+                    id="save-difficult"
+                    onClick={() => setDifficulty(index, 2)}
+                >
+                    Difficult
+                </button>
+            </div>
         </div>
     );
 };
